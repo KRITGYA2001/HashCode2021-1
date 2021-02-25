380d8b673e908caf4f4d114f618a0b8af5944d95
Peter Kvačkay <peter.kvackay@gmail.com>
2021-02-25 22:05:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
-
-#%%
-
 import networkx as nx
 from models import *
 # class crossroad:
 
 
-with open('a.txt', 'r') as inputfile:
+for letter in ["a", "b", "c", "d", "e", "f"]:
+    with open(f'{letter}.txt', 'r') as inputfile:
 
-    counter = 0
-    car_id = 0
-    streets = {}
-    cars = {}
+        counter = 0
+        car_id = 0
+        streets = {}
+        cars = {}
 
-    for line in inputfile.readlines():
-        if counter == 0:
-            duration, intersections, number_streets, number_cars, bonus = line.replace('\n','').split(' ')
-            number_streets = int(number_streets)
-            first_line = False
+        for line in inputfile.readlines():
+            if counter == 0:
+                duration, intersections, number_streets, number_cars, bonus = line.replace('\n','').split(' ')
+                number_streets = int(number_streets)
+                first_line = False
 
-        elif counter <= number_streets:
-            start_intersection, end_intersection, name, length = line.replace('\n', '').split(' ')
-            streets[name] = {'from': int(start_intersection), 'to': int(end_intersection), 'length': int(length)}
-        else:
-            car_path = line.replace('\n', '').split(' ')
-            print(type(cars))
-            cars[car_id] = car_path[1:]
-            car_id += 1
+            elif counter <= number_streets:
+                start_intersection, end_intersection, name, length = line.replace('\n', '').split(' ')
+                streets[name] = {'from': int(start_intersection), 'to': int(end_intersection), 'length': int(length)}
+            else:
+                car_path = line.replace('\n', '').split(' ')
+                print(type(cars))
+                cars[car_id] = car_path[1:]
+                car_id += 1
 
-        counter += 1
-        # streets[name] = [start_intersection, end_intersection, length]
+            counter += 1
+            # streets[name] = [start_intersection, end_intersection, length]
 
-niceCity = City(streets, int(intersections), cars)
-
-niceCity.draw()
+    niceCity = City(int(duration), streets, int(intersections), cars, letter)
+    niceCity.output()
